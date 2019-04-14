69de2ea66657b16800fe7c4dd348adf26f35bd6e
dbennerIBM <dbenner@us.ibm.com>
2019-04-14 16:42:54 -0500

    updated authentication to Postgres line 71

--- worker/src/main/java/worker/Worker.java
+++ worker/src/main/java/worker/Worker.java
@@ -68,7 +68,7 @@ class Worker {
     try {
 
       Class.forName("org.postgresql.Driver");
-      String url = "jdbc:postgresql://" + host + "/postgres";
+      String url = "jdbc:postgresql://" + host + "/postgres?user=postgres_user&password=postgres_password";
 
       while (conn == null) {
         try {
